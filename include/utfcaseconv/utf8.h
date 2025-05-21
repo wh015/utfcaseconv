71bbb25f942cbb992d50a71978ef1a58431930a6
Pavel Preblagin <pp90box@gmail.com>
2025-05-21 15:52:22 +0700

    Support SSE intrinsics for ASCII strings

--- include/utfcaseconv/utf8.h
+++ include/utfcaseconv/utf8.h
@@ -11,11 +11,22 @@
 #include <iterator>
 #include <string>
 
+#include <immintrin.h>
+
 // Such kind of UTF-8 conversion is based on
 // https://github.com/BobSteagall/utf_utils.git
 // Suitable of x86-64 or any other LE architecture.
 
 namespace utfcaseconv {
+
+static inline unsigned int ctz(unsigned int v)
+{
+#ifdef WIN32
+    return _tzcnt_u32(v);
+#else
+    return __builtin_ctz(v);
+#endif
+}
 
 template <typename IT>
 inline size_t codepoint_32to8(char32_t cdpt, IT& dst) noexcept {
@@ -65,8 +76,72 @@ inline char32_t codepoint_8to32(IT& begin, IT end) noexcept {
 }
 
 template <typename IT, typename IT2>
+inline size_t tolower_sse(IT& from, IT2& to) {
+    const char *pfrom = &(*from);
+    char *pto = &(*to);
+
+    auto src = _mm_loadu_si128((const __m128i*)pfrom);
+
+    auto ga = _mm_cmpgt_epi8(src, _mm_set1_epi8('A' - 1));
+    auto lz = _mm_cmpgt_epi8(_mm_set1_epi8('Z' + 1), src);
+
+    auto alpla = _mm_and_si128(ga, lz);
+
+    auto v0 = _mm_and_si128(alpla, _mm_set1_epi8(0x20));
+    auto v1 = _mm_xor_si128(src, v0);
+    _mm_storeu_si128((__m128i*)&(*to), v1);
+
+    auto mask = _mm_movemask_epi8(src);
+    auto count = (mask == 0) ? sizeof(__m128i) : ctz(mask);
+
+    from += count;
+    to += count;
+
+    return count;
+}
+
+template <typename IT, typename IT2>
+inline size_t toupper_sse(IT& from, IT2& to) {
+    const char *pfrom = &(*from);
+    char *pto = &(*to);
+
+    auto src = _mm_loadu_si128((const __m128i*)pfrom);
+
+    auto ga = _mm_cmpgt_epi8(src, _mm_set1_epi8('a' - 1));
+    auto lz = _mm_cmpgt_epi8(_mm_set1_epi8('z' + 1), src);
+
+    auto alpla = _mm_and_si128(ga, lz);
+
+    auto v0 = _mm_and_si128(alpla, _mm_set1_epi8(0x20));
+    auto v1 = _mm_xor_si128(src, v0);
+    _mm_storeu_si128((__m128i*)&(*to), v1);
+
+    auto mask = _mm_movemask_epi8(src);
+    auto count = (mask == 0) ? sizeof(__m128i) : ctz(mask);
+
+    from += count;
+    to += count;
+
+    return count;
+}
+
+
+template <typename IT, typename IT2>
 size_t toupper(IT begin, IT end, IT2 dst) noexcept {
     auto res = 0;
+
+    while (std::distance(begin, end) >= sizeof(__m128i)) {
+        auto octet = static_cast<uint8_t>(*begin);
+        if (octet < BORDER_ASCII) {
+            res += toupper_sse(begin, dst);
+        } else {
+            auto cdpt = codepoint_8to32(begin, end);
+            if (cdpt == CODEPOINT32_INVALID) {
+                break;
+            }
+            res += codepoint_32to8(utf32::toupper(cdpt), dst);
+        }
+    }
 
     while (begin != end) {
         auto octet = static_cast<uint8_t>(*begin);
@@ -90,6 +165,19 @@ size_t toupper(IT begin, IT end, IT2 dst) noexcept {
 template <typename IT, typename IT2>
 size_t tolower(IT begin, IT end, IT2 dst) noexcept {
     auto res = 0;
+
+    while (std::distance(begin, end) >= sizeof(__m128i)) {
+        auto octet = static_cast<uint8_t>(*begin);
+        if (octet < BORDER_ASCII) {
+            res += tolower_sse(begin, dst);
+        } else {
+            auto cdpt = codepoint_8to32(begin, end);
+            if (cdpt == CODEPOINT32_INVALID) {
+                break;
+            }
+            res += codepoint_32to8(utf32::tolower(cdpt), dst);
+        }
+    }
 
     while (begin != end) {
         auto octet = static_cast<uint8_t>(*begin);
