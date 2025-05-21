17a2dfd036b08b0606292ae9f97c7840593dabc6
Pavel Preblagin <pp90box@gmail.com>
2025-05-21 15:52:20 +0700

    Add unit tests for all bicameral alphabets

--- tests/tolower_test.cpp
+++ tests/tolower_test.cpp
@@ -2,12 +2,107 @@
 
 #include <utf32caseconv/utf32caseconv.h>
 
+using namespace utf32caseconv;
+
 TEST_CASE("Latin") {
-    REQUIRE(utf32caseconv::tolower('L') == 'l');
-    REQUIRE(utf32caseconv::tolower('x') == 'x');
+    REQUIRE(tolower(U'A') == U'a');
+    REQUIRE(tolower(U'Z') == U'z');
+    REQUIRE(tolower(U'n') == U'n');
 }
 
 TEST_CASE("Cyrillic") {
-    REQUIRE(utf32caseconv::tolower(U'Ф') == U'ф');
-    REQUIRE(utf32caseconv::tolower(U'ж') == U'ж');
+    REQUIRE(tolower(U'А') == U'а');
+    REQUIRE(tolower(U'Я') == U'я');
+    REQUIRE(tolower(U'н') == U'н');
+}
+
+TEST_CASE("Greek") {
+    REQUIRE(tolower(U'Α') == U'α');
+    REQUIRE(tolower(U'Ω') == U'ω');
+    REQUIRE(tolower(U'μ') == U'μ');
+}
+
+TEST_CASE("Coptic") {
+    REQUIRE(tolower(U'Ⲁ') == U'ⲁ');
+    REQUIRE(tolower(U'Ⳁ') == U'ⳁ');
+    REQUIRE(tolower(U'ⲗ') == U'ⲗ');
+}
+
+TEST_CASE("Armenian") {
+    REQUIRE(tolower(U'Ա') == U'ա');
+    REQUIRE(tolower(U'Բ') == U'բ');
+    REQUIRE(tolower(U'ճ') == U'ճ');
+}
+
+TEST_CASE("Glagolitic") {
+    REQUIRE(tolower(U'Ⰰ') == U'ⰰ');
+    REQUIRE(tolower(U'Ⱚ') == U'ⱚ');
+    REQUIRE(tolower(U'ⱈ') == U'ⱈ');
+}
+
+TEST_CASE("Adlam") {
+    REQUIRE(tolower(U'𞤀') == U'𞤢');
+    REQUIRE(tolower(U'𞤛') == U'𞤽');
+    REQUIRE(tolower(U'𞤯') == U'𞤯');
+    // for loanwords
+    REQUIRE(tolower(U'𞤊') == U'𞤬');
+}
+
+TEST_CASE("Warang Citi") {
+    REQUIRE(tolower(U'𑢠') == U'𑣀');
+    REQUIRE(tolower(U'𑢿') == U'𑣟');
+    REQUIRE(tolower(U'𑣏') == U'𑣏');
+}
+
+TEST_CASE("Garay") {
+    REQUIRE(tolower(U'𐵚') == U'𐵺');
+    REQUIRE(tolower(U'𐵥') == U'𐶅');
+    REQUIRE(tolower(U'𐶎') == U'𐶎');
+}
+
+TEST_CASE("Zaghawa") {
+    // only proposed to be added to Unicode
+}
+
+TEST_CASE("Osage") {
+    REQUIRE(tolower(U'𐒰') == U'𐓘');
+    REQUIRE(tolower(U'𐓃') == U'𐓫');
+    REQUIRE(tolower(U'𐓪') == U'𐓪');
+}
+
+TEST_CASE("Vithkuqi") {
+    REQUIRE(tolower(U'𐕰') == U'𐖗');
+    REQUIRE(tolower(U'𐖕') == U'𐖼');
+    REQUIRE(tolower(U'𐖙') == U'𐖙');
+}
+
+TEST_CASE("Deseret") {
+    REQUIRE(tolower(U'𐐀') == U'𐐨');
+    REQUIRE(tolower(U'𐐧') == U'𐑏');
+    REQUIRE(tolower(U'𐑅') == U'𐑅');
+}
+
+TEST_CASE("Georgian") {
+    // asomtavruli & nuskhuri
+    REQUIRE(tolower(U'Ⴀ') == U'ⴀ');
+    REQUIRE(tolower(U'Ⴥ') == U'ⴥ');
+    REQUIRE(tolower(U'ⴙ') == U'ⴙ');
+    // mkhedruli & mtavruli
+    REQUIRE(tolower(U'Ა') == U'ა');
+    REQUIRE(tolower(U'Ჰ') == U'ჰ');
+    REQUIRE(tolower(U'რ') == U'რ');
+}
+
+TEST_CASE("Non-bicameral") {
+    REQUIRE(tolower(U'غ') == U'غ');
+    REQUIRE(tolower(U'大') == U'大');
+    REQUIRE(tolower(U'東') == U'東');
+    REQUIRE(tolower(U'デ') == U'デ');
+    REQUIRE(tolower(U'❤') == U'❤');
+    REQUIRE(tolower(U'𐠔') == U'𐠔');
+    REQUIRE(tolower(U'ᚦ') == U'ᚦ');
+    REQUIRE(tolower(U'అ') == U'అ');
+    REQUIRE(tolower(U'ㄱ') == U'ㄱ');
+    REQUIRE(tolower(U'ஊ') == U'ஊ');
+    REQUIRE(tolower(U'ฌ') == U'ฌ');
 }
--- tests/toupper_test.cpp
+++ tests/toupper_test.cpp
@@ -2,12 +2,113 @@
 
 #include <utf32caseconv/utf32caseconv.h>
 
+using namespace utf32caseconv;
+
+#include <catch2/catch.hpp>
+
+#include <utf32caseconv/utf32caseconv.h>
+
+using namespace utf32caseconv;
+
 TEST_CASE("Latin") {
-    REQUIRE(utf32caseconv::toupper('a') == 'A');
-    REQUIRE(utf32caseconv::toupper('Z') == 'Z');
+    REQUIRE(toupper(U'a') == U'A');
+    REQUIRE(toupper(U'z') == U'Z');
+    REQUIRE(toupper(U'N') == U'N');
 }
 
 TEST_CASE("Cyrillic") {
-    REQUIRE(utf32caseconv::toupper(U'у') == U'У');
-    REQUIRE(utf32caseconv::toupper(U'Д') == U'Д');
+    REQUIRE(toupper(U'а') == U'А');
+    REQUIRE(toupper(U'я') == U'Я');
+    REQUIRE(toupper(U'Н') == U'Н');
+}
+
+TEST_CASE("Greek") {
+    REQUIRE(toupper(U'α') == U'Α');
+    REQUIRE(toupper(U'ω') == U'Ω');
+    REQUIRE(toupper(U'Ψ') == U'Ψ');
+}
+
+TEST_CASE("Coptic") {
+    REQUIRE(toupper(U'ⲁ') == U'Ⲁ');
+    REQUIRE(toupper(U'ⳁ') == U'Ⳁ');
+    REQUIRE(toupper(U'Ⲫ') == U'Ⲫ');
+}
+
+TEST_CASE("Armenian") {
+    REQUIRE(toupper(U'ա') == U'Ա');
+    REQUIRE(toupper(U'բ') == U'Բ');
+    REQUIRE(toupper(U'Ձ') == U'Ձ');
+}
+
+TEST_CASE("Glagolitic") {
+    REQUIRE(toupper(U'ⰰ') == U'Ⰰ');
+    REQUIRE(toupper(U'ⱚ') == U'Ⱚ');
+    REQUIRE(toupper(U'Ⱓ') == U'Ⱓ');
+}
+
+TEST_CASE("Adlam") {
+    REQUIRE(toupper(U'𞤢') == U'𞤀');
+    REQUIRE(toupper(U'𞤽') == U'𞤛');
+    REQUIRE(toupper(U'𞤐') == U'𞤐');
+    // for loanwords
+    REQUIRE(toupper(U'𞤬') == U'𞤊');
+}
+
+TEST_CASE("Warang Citi") {
+    REQUIRE(toupper(U'𑣀') == U'𑢠');
+    REQUIRE(toupper(U'𑣟') == U'𑢿');
+    REQUIRE(toupper(U'𑢫') == U'𑢫');
+}
+
+TEST_CASE("Garay") {
+    REQUIRE(toupper(U'𐵺') == U'𐵚');
+    REQUIRE(toupper(U'𐶅') == U'𐵥');
+    REQUIRE(toupper(U'𐵕') == U'𐵕');
+}
+
+TEST_CASE("Zaghawa") {
+    // only proposed to be added to Unicode
+}
+
+TEST_CASE("Osage") {
+    REQUIRE(toupper(U'𐓘') == U'𐒰');
+    REQUIRE(toupper(U'𐓫') == U'𐓃');
+    REQUIRE(toupper(U'𐓋') == U'𐓋');
+}
+
+TEST_CASE("Vithkuqi") {
+    REQUIRE(toupper(U'𐖗') == U'𐕰');
+    REQUIRE(toupper(U'𐖼') == U'𐖕');
+    REQUIRE(toupper(U'𐖄') == U'𐖄');
+}
+
+TEST_CASE("Deseret") {
+    REQUIRE(toupper(U'𐐨') == U'𐐀');
+    REQUIRE(toupper(U'𐑏') == U'𐐧');
+    REQUIRE(toupper(U'𐐘') == U'𐐘');
+}
+
+TEST_CASE("Georgian") {
+    // asomtavruli & nuskhuri
+    REQUIRE(toupper(U'ⴀ') == U'Ⴀ');
+    REQUIRE(toupper(U'ⴥ') == U'Ⴥ');
+    REQUIRE(toupper(U'Ⴑ') == U'Ⴑ');
+    // mkhedruli & mtavruli
+    REQUIRE(toupper(U'ა') == U'Ა');
+    REQUIRE(toupper(U'ჰ') == U'Ჰ');
+    REQUIRE(toupper(U'Დ') == U'Დ');
+}
+
+TEST_CASE("Non-bicameral") {
+    REQUIRE(toupper(U'غ') == U'غ');
+    REQUIRE(toupper(U'大') == U'大');
+    REQUIRE(toupper(U'東') == U'東');
+    REQUIRE(toupper(U'デ') == U'デ');
+    REQUIRE(toupper(U'❤') == U'❤');
+    REQUIRE(toupper(U'𐠔') == U'𐠔');
+    REQUIRE(toupper(U'ᚦ') == U'ᚦ');
+    REQUIRE(toupper(U'అ') == U'అ');
+    REQUIRE(toupper(U'ㄱ') == U'ㄱ');
+    REQUIRE(toupper(U'ஊ') == U'ஊ');
+    REQUIRE(toupper(U'ฌ') == U'ฌ');
 }
